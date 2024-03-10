2eb16160060c703fab25e3e6c4f0522cc49f2ca7
Dikshasharmaa <diksha66sharma78@gmail.com>
2024-03-09 19:34:54 -0500

    finished

--- Step4/ReadFour.java
+++ Step4/ReadFour.java
@@ -1,5 +1,6 @@
 package Step4;
 
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
@@ -14,5 +15,17 @@ public class ReadFour
         {
             // Print out a running total of all the
             // values in the input file.
+            try{
+                int sum =0;
+                Scanner sc = new Scanner(new File("/Users/diksha/Desktop/Projects/FileIOJava/Step4/input.txt"));
+                while(sc.hasNext()){
+                    int LineIn = sc.nextInt();
+                    sum = sum+LineIn;
+                    System.out.println("Running Total: "+sum);
+                }
+            }
+            catch (IOException e) {
+                System.out.println("File not found");
+            }
         }
 }
